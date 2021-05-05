f131ca4cdb41d1325efe421889829ffef8d56ec0
sowmyabalam <51471908+sowmyabalam@users.noreply.github.com>
2021-05-05 17:14:52 +0530

    Update pricecomparison.py

--- pricecomparison.py
+++ pricecomparison.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+headers = {'User-Agent': 'type user agent'}
 
 flipkart=''
 ebay=''
